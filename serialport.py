b21ca4c494ca29fe9ba6a82cae3855ecd5af8803
Antonio Cerruto <yonio.cerruto@gmail.com>
2024-05-25 09:47:28 +0100

    SerialPort class

--- serialport.py
+++ serialport.py
@@ -5,46 +5,67 @@ import serial
 from sys import platform
 from time import sleep
 
-BAUDRATE=250000
+class SerialPort:
+	'''
+	SerialPort class opens a serial port on MacOS or Linux.
+	Read strings from port or write strings to port.
+	'''
 
-def port_setup():
-	ser = None
-	try:
-		# list arduino ports: /dev/tty.usbmodem*
-		if platform == 'darwin':	# MACOS
-			port = glob.glob('/dev/tty.usbserial*')[0]
-		else:
-			port = '/dev/ttyUSB0'
+	def __init__(self,
+				baud=250000,
+				rtscts=False,
+				timeout=0,
+				write_timeout=0):
+		self.ser = None
+		self.port = None
+		try:
+			# list arduino ports: /dev/tty.usbmodem*
+			if platform == 'darwin':	# MACOS
+				self.port = glob.glob('/dev/tty.usbserial*')[0]
+			else:
+				self.port = '/dev/ttyUSB0'
 
-		# open serial port
-		ser = serial.Serial(port,
-							BAUDRATE,
-							rtscts=False,
-							timeout=0,
-							write_timeout=0)
-		print(ser.name) 	# check which port was really used
-		ser.reset_input_buffer()
-		ser.reset_output_buffer()
-	except:
-		print("ERROR: no port found")
-	return ser
+			# open serial port
+			self.ser = serial.Serial(self.port,
+									baudrate=baud,
+									rtscts=rtscts,
+									timeout=timeout,
+									write_timeout=write_timeout)
+			print(self.ser.name) 	# check which port was really used
+			self.ser.reset_input_buffer()
+			self.ser.reset_output_buffer()
+		except:
+			print("ERROR: no port found")
 
-def read_state():
-	if platform == 'darwin':	# MACOS
-		port = glob.glob('/dev/tty.usbserial*')[0]
-	else:
-		port = '/dev/ttyUSB0'
-	with serial.Serial(port, BAUDRATE, rtscts=True) as ser:
-		line = ser.readline()
-		print(line)
-	return line
+	def readline():
+		line = self.ser.readline()
+		return line.decode('utf-8')
+
+	def readbytes(nbytes):
+		return self.ser.read(nbytes)
+
+	def writestring(s):
+		self.ser.write(s.encode('utf-8'))
+
+	def writesbytes(b):
+		''' 
+		input in format b'\x01\x02\x03\x04'
+		'''
+		self.ser.write(b)
+
+	def close()
+		self.ser.close()
+
 
 if __name__ == "__main__":
     # execute only if run as a script
-    ser = port_setup()
-    for i in range(50):
-    	ser.write(('P'+str(i).zfill(3)).encode('utf-8'))
-    	print(f"{('P'+str(i).zfill(3)).encode('utf-8')}")
+    ser = SerialPort(baud=9600)
+    # for i in range(50):
+    	# ser.writestring(str(i))
+    while True:
+    	line = ser.readline()
+    	if line:
+    		print(line)
     ser.close()
 
 
